f708869f9806a488ddf0fc0b83c69d0cb7b1613c
Cheeseman1234123 <abrerhasanaaron13@gmail.com>
2026-01-31 15:38:00 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import cv2
 image = cv2.imread('images.jpg')
-cv2.namedWindow('Loaded Image', cv2.WINDOW_NORMAL)
-cv2.resizeWindow('Loaded Image', 500, 500)
-cv2.imshow('Loaded Image', image)
-cv2.waitKey(0)
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+resized_image = cv2.resize(gray_image, (500, 500))
+cv2.imshow('Processed Image', resized_image)
+key = cv2.waitKey(0)
+if key == ord('s'):
+    cv2.imwrite('grayscale_resized_image.jpg', resized_image)
+    print("Image saved as grayscale_resized_image.jpg")
+else:
+    print("Image not saved")
 cv2.destroyAllWindows()
-print("Image Dimension: {image.shape}")
+print(f"Processed Image Dimensions: {resized_image.shape}")
