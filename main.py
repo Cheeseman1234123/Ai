48f9b67040320018a4c520db8ab2e5f2b721869e
Cheeseman1234123 <abrerhasanaaron13@gmail.com>
2026-04-03 13:57:11 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 import requests
-url = "https://uselessfacts.jsph.pl/api/v2/facts/random?language=en"
-def get_random_technology_fact():
+general_url = "https://uselessfacts.jsph.pl/random.json?language=en"
+technology_url = "https://uselessfacts.jsph.pl/category/Technology.json?language=en"
+history_url = "https://uselessfacts.jsph.pl/category/History.json?language=en"
+science_url = "https://uselessfacts.jsph.pl/category/Science.json?language=en"
+def get_fact(url, label):
     response = requests.get(url)
     if response.status_code == 200:
         fact_data = response.json()
-        print("Did you know? " + fact_data['text'])
+        print(label + ": " + fact_data['text'])
     else:
-        print("Failed to fetch fact")
+        print("Failed to fetch text")
 while True:
-    user_input = input("Press Enter to get a random tech fact or type 'q' to quit...")
-    if user_input.lower() == 'q':
+    print("\nChoose an option:")
+    print("1. General Fact")
+    print("2. Technology Fact")
+    print("3. History Fact")
+    print("4. Science Fact")
+    print("q. Quit")
+    user_input = input("Enter your choice:")
+    if user_input.lower() == "q":
         break
-    get_random_technology_fact()
+    elif user_input == "1":
+        get_fact(general_url, "General Fact")
+    elif user_input == "2":
+        get_fact(technology_url, "Technology Fact")
+    elif user_input == "3":
+        get_fact(history_url, "History Fact")
+    elif user_input == "4":
+        get_fact(science_url, "Science Fact")
+    else:
+        print("Invalid Choice")
